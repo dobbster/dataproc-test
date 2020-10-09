4a716fee2b335ce96fae6f4d2f1e5d0c2c8bec31
dobbster <h.sinha.588@gmail.com>
2020-10-09 11:36:00 +0530

    test hello streaming 10 mins

--- hello/HelloWorld.scala
+++ hello/HelloWorld.scala
@@ -1,7 +1,7 @@
 object HelloWorld {
     def main(args: Array[String]) {
         println("Hello, world commence!")
-        Thread.sleep(10*1000)
+        Thread.sleep(10*60*1000)
         println("Hello, world end!")
     }
 }
